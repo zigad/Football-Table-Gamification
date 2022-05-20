594924575f83bf625e3f42fa61c468f05edbfdb5
Žiga Deisinger <ziga@deisinger.si>
2022-05-20 22:39:24 +0200

    Update Led.py

--- Led.py
+++ Led.py
@@ -6,13 +6,13 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 
 #setup output pins
+GPIO.setup(38, GPIO.OUT)
 GPIO.setup(24, GPIO.OUT)
-GPIO.setup(26, GPIO.OUT)
-GPIO.setup(29, GPIO.OUT)
+GPIO.setup(40, GPIO.OUT)
 GPIO.setup(32, GPIO.OUT)
 GPIO.setup(36, GPIO.OUT)
-GPIO.setup(38, GPIO.OUT)
-GPIO.setup(40, GPIO.OUT)
+GPIO.setup(26, GPIO.OUT)
+GPIO.setup(18, GPIO.OUT)
 
 #define 7 segment digits
 digitclr=[1,1,1,1,1,1,1]
@@ -26,7 +26,7 @@ digit6=[0,1,0,0,0,0,0,]
 digit7=[0,0,0,1,1,1,1]
 digit8=[0,0,0,0,0,0,0]
 digit9=[0,0,0,1,1,0,0,]
-gpin=[24,26,29,32,36,38,40]
+gpin=[38,24,40,32,36,26,18]
 #routine to clear and then write to display
 def digdisp(digit):
     for x in range (0,7):
@@ -56,6 +56,7 @@ time.sleep(1)
 digdisp(digit9)
 time.sleep(1)
 #tidy up
+
 GPIO.cleanup()
 import sys
 sys.exit()
