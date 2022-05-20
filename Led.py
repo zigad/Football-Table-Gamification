d0eac09bd37fe731bdd29756b430432036b1c01b
Žiga Deisinger <ziga@deisinger.si>
2022-05-20 22:46:07 +0200

    Update Led.py

--- Led.py
+++ Led.py
@@ -1,12 +1,9 @@
-import RPi.GPIO as IO            # calling for header file which helps us use GPIO’s of PI
+import RPi.GPIO as IO
+import time 
 
-import time                              # calling for time to provide delays in program
-
-DISPLAY = [0x3F,0x06,0x5B,0x4F,0x66,0x6D,0x7D,0x07,0x7F,0x67]            # string of characters storing PORT values for each digit.
-
-IO.setwarnings(False)            # do not show any warnings
-
-IO.setmode (IO.BCM)           # programming the GPIO by BCM pin numbers. (like PIN29 as‘GPIO5’)
+DISPLAY = [0x3F,0x06,0x5B,0x4F,0x66,0x6D,0x7D,0x07,0x7F,0x67] 
+IO.setwarnings(False)
+IO.setmode (IO.BCM)
 
 IO.setup(38, IO.OUT)
 IO.setup(24, IO.OUT)
@@ -16,24 +13,17 @@ IO.setup(36, IO.OUT)
 IO.setup(26, IO.OUT)
 IO.setup(18, IO.OUT)
 
-def PORT(pin):                    # assigning GPIO logic by taking 'pin' value
-
+def PORT(pin):
     if(pin&0x01 == 0x01):
-
-        IO.output(13,1)            # if  bit0 of 8bit 'pin' is true, pull PIN13 high
-
+        IO.output(13,1) 
     else:
-
-        IO.output(13,0)            # if  bit0 of 8bit 'pin' is false, pull PIN13 low
-
+        IO.output(13,0)
     if(pin&0x02 == 0x02):
 
-        IO.output(6,1)             # if  bit1 of 8bit 'pin' is true, pull PIN6 high
-
+        IO.output(6,1)
     else:
 
-        IO.output(6,0)            #if  bit1 of 8bit 'pin' is false, pull PIN6 low
-
+        IO.output(6,0)
     if(pin&0x04 == 0x04):
 
         IO.output(16,1)
@@ -76,20 +66,15 @@ def PORT(pin):                    # assigning GPIO logic by taking 'pin' value
 
     if(pin&0x80 == 0x80):
 
-        IO.output(12,1)            # if  bit7 of 8bit 'pin' is true, pull PIN12 high
-
+        IO.output(12,1)
     else:
 
-        IO.output(12,0)            # if  bit7 of 8bit 'pin' is false, pull PIN12 low
-
+        IO.output(12,0)
         
 
 while 1:
 
-    for x in range(10):            # execute the loop ten times incrementing x value from zero to nine
-
-        pin = DISPLAY[x]        # assigning value to 'pin' for each digit
-
-        PORT(pin);                  # showing each digit on display 
-
+    for x in range(10):
+        pin = DISPLAY[x]
+        PORT(pin)
         time.sleep(1)
