d4649ecd0d4da7ce7d334e08426e3cab9642a72c
Žiga Deisinger <ziga@deisinger.si>
2022-05-20 22:49:49 +0200

    Revert "Update Led.py"
    This reverts commit 62cd59db0fd4edcf450f830b436ce860d687a6fc.

--- Led.py
+++ Led.py
@@ -1,95 +1,62 @@
-import RPi.GPIO as IO            # calling for header file which helps us use GPIO’s of PI
+import RPi.GPIO as GPIO
+import time
+import os, sys
 
-import time                              # calling for time to provide delays in program
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
 
-DISPLAY = [0x3F,0x06,0x5B,0x4F,0x66,0x6D,0x7D,0x07,0x7F,0x67]            # string of characters storing PORT values for each digit.
+#setup output pins
+GPIO.setup(38, GPIO.OUT)
+GPIO.setup(24, GPIO.OUT)
+GPIO.setup(40, GPIO.OUT)
+GPIO.setup(32, GPIO.OUT)
+GPIO.setup(36, GPIO.OUT)
+GPIO.setup(26, GPIO.OUT)
+GPIO.setup(18, GPIO.OUT)
 
-IO.setwarnings(False)            # do not show any warnings
+#define 7 segment digits
+digitclr=[1,1,1,1,1,1,1]
+digit0=[0,0,0,0,0,0,1]
+digit1=[1,0,0,1,1,1,1]
+digit2=[0,0,1,0,0,1,0]
+digit3=[0,0,0,0,1,1,0]
+digit4=[1,0,0,1,1,0,0]
+digit5=[0,1,0,0,1,0,0]
+digit6=[0,1,0,0,0,0,0,]
+digit7=[0,0,0,1,1,1,1]
+digit8=[0,0,0,0,0,0,0]
+digit9=[0,0,0,1,1,0,0,]
+gpin=[38,24,40,32,36,26,18]
+#routine to clear and then write to display
+def digdisp(digit):
+    for x in range (0,7):
+        GPIO.output(gpin[x], digitclr[x])
+    for x in range (0,7):
+        GPIO.output(gpin[x], digit[x])
+#routine to display digit from 0 to 9
 
-IO.setmode (IO.BCM)           # programming the GPIO by BCM pin numbers. (like PIN29 as‘GPIO5’)
+digdisp(digit0)
+time.sleep(1)
+digdisp(digit1)
+time.sleep(1)
+digdisp(digit2)
+time.sleep(1)
+digdisp(digit3)
+time.sleep(1)
+digdisp(digit4)
+time.sleep(1)
+digdisp(digit5)
+time.sleep(1)
+digdisp(digit6)
+time.sleep(1)
+digdisp(digit7)
+time.sleep(1)
+digdisp(digit8)
+time.sleep(1)
+digdisp(digit9)
+time.sleep(1)
+#tidy up
 
-IO.setup(38, IO.OUT)
-IO.setup(24, IO.OUT)
-IO.setup(40, IO.OUT)
-IO.setup(32, IO.OUT)
-IO.setup(36, IO.OUT)
-IO.setup(26, IO.OUT)
-IO.setup(18, IO.OUT)
-
-def PORT(pin):                    # assigning GPIO logic by taking 'pin' value
-
-    if(pin&0x01 == 0x01):
-
-        IO.output(13,1)            # if  bit0 of 8bit 'pin' is true, pull PIN13 high
-
-    else:
-
-        IO.output(13,0)            # if  bit0 of 8bit 'pin' is false, pull PIN13 low
-
-    if(pin&0x02 == 0x02):
-
-        IO.output(6,1)             # if  bit1 of 8bit 'pin' is true, pull PIN6 high
-
-    else:
-
-        IO.output(6,0)            #if  bit1 of 8bit 'pin' is false, pull PIN6 low
-
-    if(pin&0x04 == 0x04):
-
-        IO.output(16,1)
-
-    else:
-
-        IO.output(16,0)
-
-    if(pin&0x08 == 0x08):
-
-        IO.output(20,1)
-
-    else:
-
-        IO.output(20,0)   
-
-    if(pin&0x10 == 0x10):
-
-        IO.output(21,1)
-
-    else:
-
-        IO.output(21,0)
-
-    if(pin&0x20 == 0x20):
-
-        IO.output(19,1)
-
-    else:
-
-        IO.output(19,0)
-
-    if(pin&0x40 == 0x40):
-
-        IO.output(26,1)
-
-    else:
-
-        IO.output(26,0)
-
-    if(pin&0x80 == 0x80):
-
-        IO.output(12,1)            # if  bit7 of 8bit 'pin' is true, pull PIN12 high
-
-    else:
-
-        IO.output(12,0)            # if  bit7 of 8bit 'pin' is false, pull PIN12 low
-
-        
-
-while 1:
-
-    for x in range(10):            # execute the loop ten times incrementing x value from zero to nine
-
-        pin = DISPLAY[x]        # assigning value to 'pin' for each digit
-
-        PORT(pin);                  # showing each digit on display 
-
-        time.sleep(1)
+GPIO.cleanup()
+import sys
+sys.exit()
